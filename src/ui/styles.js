9dc77ca1766b5015bdab5582c34d45e5f10fae40
Kevin Rodríguez <_@kevinrodriguez.io>
2019-05-29 10:46:04 -0600

    Create styles.js

---
+++ src/ui/styles.js
@@ -0,0 +1,12 @@
+export const centerBlock = {
+  marginRight: 'auto',
+  marginLeft: 'auto',
+};
+
+export const mt1 = {
+  marginTop: '1em'
+};
+
+export const height100vh = {
+  height: '100vh'
+};
