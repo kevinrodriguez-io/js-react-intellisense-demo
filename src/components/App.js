2c9abf5a00c3b3fbd1c6264fa74721a52e71aee5
Kevin Rodríguez <_@kevinrodriguez.io>
2019-05-29 10:46:27 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PersonCard from './PersonCard';
-import { centerBlock, mt1 } from '../ui/align-styles';
+import { centerBlock, mt1, height100vh } from '../ui/styles';
+import { Grid, GridRow, Loader, Segment, Dimmer } from 'semantic-ui-react';
 
 export default () => {
-  return <PersonCard
-    style={{ ...centerBlock, ...mt1 }}
-    name="Kevin"
-    age={24}
-    photoUrl="https://avatars3.githubusercontent.com/u/6248571"
-  />;
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const json = await response.json();
+    setUsers(json);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+
+  if (users.length === 0) {
+    return (
+      <Segment style={height100vh}>
+        <Dimmer active>
+          <Loader size='small'>Loading</Loader>
+        </Dimmer>
+      </Segment>
+    );
+  }
+
+  return (
+    <Grid relaxed padded>
+      <GridRow>
+        {users.map(user =>
+          <PersonCard
+            style={{ ...centerBlock, ...mt1 }}
+            name={user.name}
+            email={user.email}
+            photoUrl="http://placehold.it/500x300"
+          />)
+        }
+      </GridRow>
+    </Grid>
+  );
 };
